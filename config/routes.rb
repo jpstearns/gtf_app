7253423a6df6c49ca8a9486ce79dc972273135c0
jpstearns <jared.patrick.stearns>
2015-02-16 11:14:14 -0800

    run devise gem to handle users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  devise_for :installs
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
