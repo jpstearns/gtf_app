8b49b6e2dc58a0bf60d7bd2ceaa35dbfb05c55a3
jps <jared.patrick.stearns@gmail.com>
2015-04-19 20:56:09 -0400

    Update Login page

--- db/migrate/20150419001225_add_fields_to_user.rb
+++ db/migrate/20150419001225_add_fields_to_user.rb
@@ -2,5 +2,10 @@ class AddFieldsToUser < ActiveRecord::Migration
   def change
   	    add_column :users, :first_name, :string
         add_column :users, :last_name, :string
+        add_column :users, :badge_number, :string
+        add_column :users, :phone_number, :string
+        add_column :users, :desk_extenstion, :number
+        add_column :users, :rank, :string
+
   end
 end
