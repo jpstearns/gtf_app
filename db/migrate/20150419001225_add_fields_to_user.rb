1d547afa52b2ebaeb5510392448d5fa47ddb9b6a
jps <jared.patrick.stearns@gmail.com>
2015-04-22 06:13:58 -0400

    fix data migration errors

--- db/migrate/20150419001225_add_fields_to_user.rb
+++ db/migrate/20150419001225_add_fields_to_user.rb
@@ -3,8 +3,8 @@ class AddFieldsToUser < ActiveRecord::Migration
   	    add_column :users, :first_name, :string
         add_column :users, :last_name, :string
         add_column :users, :badge_number, :string
-        add_column :users, :phone_number, :number
-        add_column :users, :desk_extenstion, :number
+        add_column :users, :phone_number, :string
+        add_column :users, :desk_extenstion, :string
         add_column :users, :rank, :string
   end
 end
