4d3362de73ee5201542103a69b08d5f076e94840
jps <jared.patrick.stearns@gmail.com>
2015-04-23 11:48:55 -0400

    validate client data entry.

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,2 +1,8 @@
 class Applicant < ActiveRecord::Base
+	validates_presence_of :first_name, :last_name, :address, :city, :state, :social, :message => "Missing a required field" 
+	validates_numericality_of :zip, message: "Zip Code must be a number"
+	validates_numericality_of :social, message: "Invalid Social Security Number"
+	validates_length_of :zip, is: 5, message: "Zip Code must be 5 digits"
+	validates_length_of :social, is: 9, message: "Invalid Social Security Number"
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,3 @@
 class Client < ActiveRecord::Base
+	validates_presence_of :clientName, :address1, :city, :state, :message => "Missing a required field" 
 end
