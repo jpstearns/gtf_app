d9e6caa546d358475ba242fbec7b81fb143be561
jps <jared.patrick.stearns@gmail.com>
2015-04-27 04:59:49 -0400

    calendar styled and in place for functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427015024) do
+ActiveRecord::Schema.define(version: 20150426203522) do
 
   create_table "applicants", force: :cascade do |t|
     t.string   "first_name"
@@ -33,11 +33,6 @@ ActiveRecord::Schema.define(version: 20150427015024) do
     t.datetime "updated_at",        null: false
   end
 
-  create_table "assignments", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "clients", force: :cascade do |t|
     t.string   "clientName"
     t.string   "clientPhone"
@@ -50,29 +45,6 @@ ActiveRecord::Schema.define(version: 20150427015024) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
-
-  create_table "fullcalendar_engine_event_series", force: :cascade do |t|
-    t.integer  "frequency",  default: 1
-    t.string   "period",     default: "monthly"
-    t.datetime "starttime"
-    t.datetime "endtime"
-    t.boolean  "all_day",    default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "fullcalendar_engine_events", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "starttime"
-    t.datetime "endtime"
-    t.boolean  "all_day",         default: false
-    t.text     "description"
-    t.integer  "event_series_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "fullcalendar_engine_events", ["event_series_id"], name: "index_fullcalendar_engine_events_on_event_series_id"
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
