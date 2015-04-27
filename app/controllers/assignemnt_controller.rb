668dcbe70cbbe4a5959ec0242dfaef47cc4b4de8
jps <jared.patrick.stearns@gmail.com>
2015-04-27 06:47:15 -0400

    Got my app on the cloud just in time to fall asleep at work

--- app/controllers/assignemnt_controller.rb
+++
@@ -1,2 +0,0 @@
-class AssignemntController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get 'pages/calendar'
+
   get 'assignments/show'
 
   get 'calendar/show'
